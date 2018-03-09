32826b4e831f96c227db7a421925233aa5336f9b
malsham1567 <35948110+malsham1567@users.noreply.github.com>
2018-03-08 23:56:22 -0500

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -2,54 +2,50 @@
 """
 Created on Thu Jan 14 13:44:00 2016
 Updated Jan 21, 2018
+
 The primary goal of this file is to demonstrate a simple python program to classify triangles
+
 @author: jrr
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
-    """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
-    
-    This function returns a string with the type of triangle from three integer values
-    corresponding to the lengths of the three sides of the Triangle.
-    
-    return:
-        If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
-        If no pair of  sides are equal, return 'Scalene'
-        If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
-      BEWARE: there may be a bug or two in this code
-    """
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    # I rearrange the if statements. Because we have to check to make sure the input is not strign
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-    
-    # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
-        
-    if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput'
-      
-    # This information was not in the requirements spec but 
-    # is important for correctness
-    # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
-        return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and b == c:
-        return 'Equilateral'
-    elif ((a * a) + (b * b)) == (c * c) or ((a * a) + (c * c)) == (b * b) or ((c * c) + (b * b)) == (a * a):
-        return 'Right'
-    elif (a != b) and  (b != c) and (a != c):
-        return 'Scalene'
+
+def classify_triangle(aaa, bbb, ccc):
+    try:
+        if aaa > 200 or bbb > 200 or ccc > 200:
+            return 'InvalidInput'
+
+        if aaa <= 0 or bbb <= 0 or ccc <= 0:
+            return 'InvalidInput'
+
+        if (aaa >= (bbb + ccc)) or (bbb >= (aaa + ccc)) or (ccc >= (aaa + bbb)):
+            return 'NotATriangle'
+
+    except TypeError:
+        return "InvalidInput"
+
+    if round((aaa * aaa) + (bbb * bbb)) == round(ccc * ccc) or \
+            round((bbb * bbb) + (ccc * ccc)) == round(aaa * aaa) or \
+            round((aaa * aaa) + (ccc * ccc)) == round(bbb * bbb):
+        if (aaa != bbb) and (bbb != ccc) and (aaa != ccc):
+            return 'Right Scalene Triangle'
+        else:
+            return 'Right Isosceles Triangle'
     else:
-        return 'Isosceles'
+        if (aaa != bbb) and (bbb != ccc) and (aaa != ccc):
+            return 'Scalene Triangle'
+        elif aaa == bbb and bbb == ccc:
+            return 'Equilateral Triangle'
+        else:
+            return 'Isosceles Triangle'
+
+
+def main():
+    abe = float(input('Please enter side 1: '))
+    bsd = float(input('Please enter side 2: '))
+    cjk = float(input('Please enter side 3: '))
+    print(classify_triangle(abe, bsd, cjk))
+
+
+if __name__ == '__main__':
+    main()
